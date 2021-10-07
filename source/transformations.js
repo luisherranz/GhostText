8b4b62fd9efab62acad2ae24f78fc76b1bbf492d
Luis Herranz <luisherranz@gmail.com>
2021-10-07 09:50:44 +0100

    Remove WordPress
    Using the markdown block for now.

--- source/transformations.js
+++ source/transformations.js
@@ -64,18 +64,18 @@ export default {
 	},
 
 	// WordPress.com (Gutenberg).
-	"[a-z-]+\\.wordpress.com": {
-		send: (html) => {
-			return (
-				htmlToMarkdown(html)
-					// Reset empty block.
-					.replace(/^.\n$/, "")
-			);
-		},
-		receive: (md) => {
-			return markdownToHtml(md);
-		},
-	},
+	// "(?!systemsrequests)\\.wordpress.com": {
+	// 	send: (html) => {
+	// 		return (
+	// 			htmlToMarkdown(html)
+	// 				// Reset empty block.
+	// 				.replace(/^.\n$/, "")
+	// 		);
+	// 	},
+	// 	receive: (md) => {
+	// 		return md;
+	// 	},
+	// },
 
 	// Slack.
 	"app\\.slack\\.com": {
